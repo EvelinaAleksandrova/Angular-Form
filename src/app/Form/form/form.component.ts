c19bc58e34173878585b574bd2c37fd03075eb26
evelina <evety.1dtvd@gmail.com>
2022-06-26 16:27:36 +0300

    Improved select to use Material-select. Add another json file for gender data. Improved email validation to use only pattern, not call other function. Fixed unnecessary spans and paragraphs with Mat-error to show errors.

--- src/app/Form/form/form.component.ts
+++ src/app/Form/form/form.component.ts
@@ -1,8 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
-// import {MatButtonModule} from '@angular/material/button';
-
 import hobby from 'src/assets/json/hobbies.json';
+import gender from 'src/assets/json/genders.json';
 
 @Component({
   selector: 'app-form',
@@ -12,7 +11,7 @@ import hobby from 'src/assets/json/hobbies.json';
 export class FormComponent implements OnInit {
   @ViewChild('f', {static: false}) signupForm: NgForm;
   hobbies: { code: Number, label: string }[] = hobby;
-  genders = ["Female", "Man"];
+  genders: { code: Number, label: string }[] = gender;
 
   constructor() {
 
@@ -22,40 +21,9 @@ export class FormComponent implements OnInit {
 
   }
 
-  user = {
-    firstname: '',
-    lastname: '',
-    email: '',
-    gender: '',
-    hobby: '',
-    conditions: '',
-  };
-  submitted = false;
-  isValidEmailAfterTyping;
-  isChecked;
-
-  isValidEmail(email) {
-    const regex = /^[\w+\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    regex.test(email) ? this.isValidEmailAfterTyping = true : this.isValidEmailAfterTyping = false;
-  }
-
-  onCheckConditions(event) {
-    if (event.target.checked) {
-      this.isChecked = true;
-    } else {
-      this.isChecked = false;
-    }
-  }
-
   onSubmit() {
-    this.submitted = true;
-    this.user.firstname = this.signupForm.value.userData.firstname;
-    this.user.lastname = this.signupForm.value.userData.lastname;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.gender = this.signupForm.value.userData.gender;
-    this.user.hobby = this.signupForm.value.userData.hobby;
-    this.user.conditions = this.signupForm.value.userData.conditions;
-    this.signupForm.reset();
-    console.log(this.signupForm.value.userData);
+    // console.log(this.signupForm.value);
+    // this.signupForm.reset();
+    window.location.reload();
   }
 }
