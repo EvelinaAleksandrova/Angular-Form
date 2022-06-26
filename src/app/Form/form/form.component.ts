01f5c277f84771f8995ae3eff2504b3581733f95
evelina <evety.1dtvd@gmail.com>
2022-06-26 17:23:26 +0300

    Fixed ViewChild static to be true.

--- src/app/Form/form/form.component.ts
+++ src/app/Form/form/form.component.ts
@@ -9,7 +9,7 @@ import gender from 'src/assets/json/genders.json';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  @ViewChild('f', {static: false}) signupForm: NgForm;
+  @ViewChild('f', {static: true}) signupForm: NgForm;
   hobbies: { code: Number, label: string }[] = hobby;
   genders: { code: Number, label: string }[] = gender;
 
